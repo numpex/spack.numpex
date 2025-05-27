1ffc58757b28600fd51456ef42712a5b24d6f411
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2025-05-27 10:26:30 +0200

    update Parmetis package: add version 4.0.4-rc.4, deprecate 4.0.4-rc.3 and 4.0.4-rc.2; enhance dependencies and variants

--- packages/parmetis/package.py
+++ packages/parmetis/package.py
@@ -17,7 +17,9 @@ class Parmetis(CMakePackage):
     url = "https://github.com/numpex/ParMETIS/archive/refs/tags/v4.0.4-rc.2.tar.gz"
     git = "https://github.com/numpex/ParMETIS"
 
-    version("4.0.4-rc.2", sha256="dec52ed338c1a00202659e01aff0789d4509e0cc318d073015ea2a8a79629256")
+    version("4.0.4-rc.4", sha256="f122838f1ac7e890450b9b98b6afd97f14af1cbb94be20b8630e2a7bd423ad4d",preferred=True)
+    version("4.0.4-rc.3", sha256="51d47b659e44a90358f8ca17da6d9ecaecb7be170df58e58fbb4366a85a6c996", deprecated=True)
+    version("4.0.4-rc.2", sha256="dec52ed338c1a00202659e01aff0789d4509e0cc318d073015ea2a8a79629256",deprecated=True)
 #    version("4.0.3", sha256="f2d9a231b7cf97f1fee6e8c9663113ebf6c240d407d3c118c55b3633d6be6e5f")
 #    version("4.0.2", sha256="5acbb700f457d3bda7d4bb944b559d7f21f075bb6fa4c33f42c261019ef2f0b2")
 
@@ -25,17 +27,18 @@ class Parmetis(CMakePackage):
     variant("gdb", default=False, description="Enables gdb support.")
     variant("int64", default=False, description="Sets the bit width of METIS's index type to 64.")
     variant("real64", default=False, description="Sets the bit width of METIS's real type to 64.")
+    variant("omp", default=False, description="Enables OpenMP support")
     depends_on("c", type="build")
     depends_on("cxx", type="build")
 
     depends_on("cmake@2.8:", type="build")
     depends_on("mpi")
-    depends_on("metis@5:")
-    depends_on("metis+int64", when="+int64")
-    depends_on("metis~int64", when="~int64")
+    depends_on("metis@5:", type=("build", "link", "run"))
+    depends_on("metis+int64", when="+int64", type=("build", "link", "run"))
+    depends_on("metis~int64", when="~int64", type=("build", "link", "run"))
     depends_on("gklib", type=("build", "link", "run"))
-    depends_on("metis+real64", when="+real64")
-    depends_on("metis~real64", when="~real64")
+    depends_on("metis+real64", when="+real64", type=("build", "link", "run"))
+    depends_on("metis~real64", when="~real64", type=("build", "link", "run"))
 
 
     def cmake_args(self):
